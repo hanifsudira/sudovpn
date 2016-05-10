ff441c31e73dc79607599564c2e0eaa60a9ba198
adeilhamfajri <adeilhamfajri.1995@gmail.com>
2016-05-10 17:02:22 +0700

    Plans clear.

--- web/application/views/client/plans.php
+++ web/application/views/client/plans.php
@@ -1,59 +1,65 @@
 
-<body>
-<div id="menu-holder">
-<div class="price_table">
-	<div class="column_1">
-		<ul>
-			<li class="header_row_1 align_center">
-				<div class="pack-title">Basic</div>
-			</li>
-			<li class="header_row_2 align_center">
-				<div class="price"><span>Rp 40rb</span></div>
-				<div class="time">----</div>
-			</li>
-			<li class="row_style_1 align_center"><span>Lapangan Futsal</span></li>
-			<li class="row_style_1 align_center"><span>Bola Futsal</span></li>
-			<li class="row_style_1 align_center"><span>Timer</span></li>
-			<li class="row_style_1 align_center no-option"><span>Deker Kaki</span></li>
-			<li class="row_style_1 align_center no-option"><span>Uniform</span></li>
-			<li class="row_style_footer_1"><a href="file:///C:/Xampp/htdocs/apsi/data_diri.html" class="buy_now">Lanjutkan</a></li>
-		</ul>
-	</div><!--end column-->
 
-	<div class="column_1">
-		<ul>
-			<li class="header_row_1 align_center">
-				<div class="pack-title">Premium</div>
-			</li>
-			<li class="header_row_2 align_center">
-				<div class="price"><span>Rp 60rb</span></div>
-				<div class="time">----</div>
-			</li>
-			<li class="row_style_2 align_center"><span>Lapangan Futsal</span></li>
-			<li class="row_style_2 align_center"><span>Bola Futsal</span></li>
-			<li class="row_style_2 align_center"><span>Timer</span></li>
-			<li class="row_style_2 align_center"><span>Deker Kaki</span></li>
-			<li class="row_style_2 align_center no-option"><span>Uniform</span></li>
-			<li class="row_style_footer_2"><a href="#" class="buy_now">Lanjutkan</a></li>
-		</ul>
-	</div><!--end column-->
+	<div id="menu-holder">
+		<div class="price_table">
+			<div class="column_1">
+				<ul>
+					<li class="header_row_1 align_center">
+						<div class="pack-title">Basic</div>
+					</li>
+					<li class="header_row_2 align_center">
+						<div class="price"><span>Rp 40k</span></div>
+						<div class="time">----</div>
+					</li>
+					<li class="row_style_1 align_center"><span><b>1 Credit</b></span></li>
+					<li class="row_style_1 align_center"><span>Limited Bandwidth</span></li>
+					<li class="row_style_1 align_center no-option"><span>Secure Connection</span></li>
+					<li class="row_style_1 align_center no-option"><span>Anonymous</span></li>
+					<li class="row_style_1 align_center no-option"><span>Unblock Websites</span></li>
+					<li class="row_style_1 align_center no-option"><span>Play Game Online</span></li>
+					<li class="row_style_1 align_center no-option"><span>Streaming Videos</span></li>
+					<li class="row_style_footer_1"><a href="file:///C:/Xampp/htdocs/apsi/data_diri.html" class="buy_now">Choose</a></li>
+				</ul>
+			</div><!--end column-->
 
-	<div class="column_1">
-		<ul>
-			<li class="header_row_1 align_center">
-				<div class="pack-title">Ultimate</div>
-			</li>
-			<li class="header_row_2 align_center">
-				<div class="price"><span>Rp 80rb</span></div>
-				<div class="time">----</div>
-			</li>
-			<li class="row_style_1 align_center"><span>Lapangan Futsal</span></li>
-			<li class="row_style_1 align_center"><span>Bola Futsal</span></li>
-			<li class="row_style_1 align_center"><span>Timer</span></li>
-			<li class="row_style_1 align_center"><span>Deker Kaki</span></li>
-			<li class="row_style_1 align_center"><span>Uniform</span></li>
-			<li class="row_style_footer_1"><a href="#" class="buy_now">Lanjutkan</a></li>
-		</ul>
-	</div><!--end column-->
-</div><!--end price table-->
-</div>
+			<div class="column_1">
+				<ul>
+					<li class="header_row_1 align_center">
+						<div class="pack-title">Premium</div>
+					</li>
+					<li class="header_row_2 align_center">
+						<div class="price"><span>Rp 60k</span></div>
+						<div class="time">----</div>
+					</li>
+					<li class="row_style_1 align_center"><span><b>2 Credit</b></span></li>
+					<li class="row_style_1 align_center"><span>Unlimited Bandwidth</span></li>
+					<li class="row_style_1 align_center no-option"><span>Secure Connection</span></li>
+					<li class="row_style_1 align_center no-option"><span>Anonymous</span></li>
+					<li class="row_style_1 align_center no-option"><span>Unblock Websites</span></li>
+					<li class="row_style_1 align_center no-option"><span>Play Game Online</span></li>
+					<li class="row_style_1 align_center no-option"><span>Streaming Videos</span></li>
+					<li class="row_style_footer_2"><a href="#" class="buy_now">Choose</a></li>
+				</ul>
+			</div><!--end column-->
+
+			<div class="column_1">
+				<ul>
+					<li class="header_row_1 align_center">
+						<div class="pack-title">Ultimate</div>
+					</li>
+					<li class="header_row_2 align_center">
+						<div class="price"><span>Rp 80k</span></div>
+						<div class="time">----</div>
+					</li>
+					<li class="row_style_1 align_center"><span><b>3 Credit</b></span></li>
+					<li class="row_style_1 align_center"><span>Unlimited Bandwidth</span></li>
+					<li class="row_style_1 align_center no-option"><span>Secure Connection</span></li>
+					<li class="row_style_1 align_center no-option"><span>Anonymous</span></li>
+					<li class="row_style_1 align_center no-option"><span>Unblock Websites</span></li>
+					<li class="row_style_1 align_center no-option"><span>Play Game Online</span></li>
+					<li class="row_style_1 align_center no-option"><span>Streaming Videos</span></li>
+					<li class="row_style_footer_1"><a href="#" class="buy_now">Choose</a></li>
+				</ul>
+			</div><!--end column-->
+		</div><!--end price table-->
+	</div>
