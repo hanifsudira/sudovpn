96e73083ef2a047d9a534375f3b42d169217e541
adeilhamfajri <adeilhamfajri.1995@gmail.com>
2016-05-10 07:46:07 +0700

    ngandat

--- web/application/config/config.php
+++ web/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://sudovpn.id';
+$config['base_url'] = 'http://localhost:5121/cloud/sudovpn/web';
 /*
 |--------------------------------------------------------------------------
 | Index File
