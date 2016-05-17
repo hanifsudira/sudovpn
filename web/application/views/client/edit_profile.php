f88d53ddc6f738e10abe87c3f44d48a9453d691f
ghulamfajri <ghulamfajri13@gmail.com>
2016-05-18 05:00:22 +0700

    Update view client

--- web/application/views/client/edit_profile.php
+++ web/application/views/client/edit_profile.php
@@ -9,12 +9,12 @@
 <!-- Content Header (Page header) -->
 <section class="content-header">
   <h1>
-	Edit Profile
+  Edit Profile
   </h1>
   <ol class="breadcrumb">
-	<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-	<li><a href="#">Examples</a></li>
-	<li class="active">Edit profile</li>
+  <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+  <li><a href="#">Examples</a></li>
+  <li class="active">Edit profile</li>
   </ol>
 </section>
  
@@ -26,18 +26,17 @@
         <div class='span4 offset4'>
           <form accept-charset="UTF-8" action="http://sudovpn.id:5002/updateUser" class="new_user" id="new_user" method="post"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /><input name="authenticity_token" type="hidden" value="83XB3tOTNc32k3lTEDQcXvHfs1TRPTYQupERDaRSlx8=" /></div>
 
-			<br><br>
+      <br><br>
             <label class="control-label" for="inputEmail">Full Name</label>
-            <input name="name" type="text" id="inputName" value="<?php echo $list['fullname'];?>" required>
+            <input name="fullname" type="text" id="inputName" value="<?php echo $list['fullname'];?>" required>
 
             <label class="control-label" for="inputEmail">Phone</label>
             <input name="phone" type="text" id="inputEmail" value="<?php echo $list['phone'];?>" required>
 
             <label class="control-label" for="inputEmail">Address</label>
             <input name="address" type="text" id="inputEmail" value="<?php echo $list['address'];?>" required>
-
-            <label class="control-label" for="inputPassword">Password</label>
-            <input name="passwd" type="password" id="inputPassword" value="Input Your Password!" required>
+      
+      <input name="id_user" type="hidden" id="inputEmail" value="<?php echo $list['id_user'];?>" required>
 
             <br>
             <input class="bttn bttn-green button button-small" name="commit" type="submit" value="Update"/>
